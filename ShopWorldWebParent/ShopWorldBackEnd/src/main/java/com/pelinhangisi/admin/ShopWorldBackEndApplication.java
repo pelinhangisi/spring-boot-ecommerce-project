79f161781b9123f2baff31296f1d2fff586953aa
Pelin Hangişi <pelinhangisi@gmail.com>
2022-10-04 22:58:22 +0300

    modules created

---
+++ ShopWorldWebParent/ShopWorldBackEnd/src/main/java/com/pelinhangisi/admin/ShopWorldBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.pelinhangisi.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopWorldBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopWorldBackEndApplication.class, args);
+    }
+
+}
---
+++ ShopWorldWebParent/ShopWorldFrontEnd/src/main/java/com/pelinhangisi/site/MainController.java
@@ -0,0 +1,13 @@
+package com.pelinhangisi.site;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("")
+    public String viewHomePage(){
+        return "index";
+    }
+}
