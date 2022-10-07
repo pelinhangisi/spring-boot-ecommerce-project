9a7e7e4a1fc9af0a5afc83b47859429be6a854e4
Pelin Hangişi <pelinhangisi@gmail.com>
2022-10-08 00:58:22 +0300

    Role entity created

--- ShopWorldWebParent/ShopWorldBackEnd/src/main/java/com/pelinhangisi/admin/ShopWorldBackEndApplication.java
+++ ShopWorldWebParent/ShopWorldBackEnd/src/main/java/com/pelinhangisi/admin/ShopWorldBackEndApplication.java
@@ -2,8 +2,10 @@ package com.pelinhangisi.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.pelinhangisi.shopworldcommon.entity", "com.pelinhangisi.admin.user"})
 public class ShopWorldBackEndApplication {
 
     public static void main(String[] args) {
---
+++ ShopWorldWebParent/ShopWorldBackEnd/src/test/java/com/pelinhangisi/admin/user/RoleRepositoryTests.java
@@ -0,0 +1,25 @@
+package com.pelinhangisi.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import com.pelinhangisi.shopworldcommon.entity.Role;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class RoleRepositoryTests {
+
+    @Autowired
+    private RoleRepository repo;
+
+
+    @Test
+    public void testCreateFirstRole(){
+        Role roleAdmin = new Role("Admin", "manage everything");
+        Role savedRole = repo.save(roleAdmin);
+        assertThat(savedRole.getId()).isGreaterThan(0);
+    }
+}
