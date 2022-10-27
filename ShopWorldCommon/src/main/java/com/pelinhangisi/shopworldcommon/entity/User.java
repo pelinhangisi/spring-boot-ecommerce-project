ef409e16d185d8c31a1b464b9f5ed3e443a23528
Pelin Hangişi <pelinhangisi@gmail.com>
2022-10-28 00:49:12 +0300

    coding the Create New User functionality of User module

--- ShopWorldCommon/src/main/java/com/pelinhangisi/shopworldcommon/entity/User.java
+++ ShopWorldCommon/src/main/java/com/pelinhangisi/shopworldcommon/entity/User.java
@@ -111,4 +111,18 @@ public class User {
     public void addRole(Role role){
         this.roles.add(role);
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", photos='" + photos + '\'' +
+                ", enabled=" + enabled +
+                ", roles=" + roles +
+                '}';
+    }
 }
--- ShopWorldWebParent/ShopWorldBackEnd/src/main/java/com/pelinhangisi/admin/user/UserService.java
+++ ShopWorldWebParent/ShopWorldBackEnd/src/main/java/com/pelinhangisi/admin/user/UserService.java
@@ -1,5 +1,6 @@
 package com.pelinhangisi.admin.user;
 
+import com.pelinhangisi.shopworldcommon.entity.Role;
 import com.pelinhangisi.shopworldcommon.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,20 @@ import java.util.List;
 public class UserService {
 
     @Autowired
-    private UserRepository repo;
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     public List<User> listAll(){
-        return (List<User>) repo.findAll();
+        return (List<User>) userRepository.findAll();
+    }
+
+    public List<Role> listRoles(){
+        return (List<Role>) roleRepository.findAll();
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
     }
 }
