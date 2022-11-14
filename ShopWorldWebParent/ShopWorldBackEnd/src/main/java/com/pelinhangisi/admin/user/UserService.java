b12a068f1ba657592c51253691d96c98079b5618
Pelin Hangişi <pelinhangisi@gmail.com>
2022-11-15 00:39:00 +0300

    encode user password

--- ShopWorldWebParent/ShopWorldBackEnd/src/main/java/com/pelinhangisi/admin/user/UserService.java
+++ ShopWorldWebParent/ShopWorldBackEnd/src/main/java/com/pelinhangisi/admin/user/UserService.java
@@ -3,6 +3,7 @@ package com.pelinhangisi.admin.user;
 import com.pelinhangisi.shopworldcommon.entity.Role;
 import com.pelinhangisi.shopworldcommon.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +17,9 @@ public class UserService {
     @Autowired
     private RoleRepository roleRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public List<User> listAll(){
         return (List<User>) userRepository.findAll();
     }
@@ -25,6 +29,12 @@ public class UserService {
     }
 
     public void save(User user) {
+        encodePassword(user);
         userRepository.save(user);
+    }
+
+    private void encodePassword(User user){
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
     }
 }
